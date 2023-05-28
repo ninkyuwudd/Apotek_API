5b7f9a85fe270e5dd4c87d8e120027b7bd41fdc3
NinKyuWudd <116327302+ninkyuwudd@users.noreply.github.com>
2023-05-28 14:07:54 +0700

    first commit

---
+++ src/api.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import apotekRoute from "../src/routes/apotek";
+
+export const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
+
+// Healthcheck endpoint
+app.get('/', (req, res) => {
+  res.status(200).send({ status: 'ok' });
+});
+
+
+app.use("/apotek",apotekRoute)
+
